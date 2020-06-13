ae0e5bd511eda326fd5fe7d57a1f6897b52a114d
Ciro Botelho Lacerda <cirolacerda@gmail.com>
2020-06-13 13:56:56 -0300

    Reestruturando arquivos

--- server.js
+++ server.js
@@ -12,17 +12,8 @@ mongoose.connect(
 );
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
 
-// Primeira rota
-app.get("/", (req, res) => {
-    Product.create({
-        title: 'React Native',
-        description: 'Build native apps with React',
-        url: 'http://github.com/facebook/react-native'
-    });
-
-   return res.send("Hello World Ciro");
-});
+// Rotas
+app.use("/api", require("./src/routes"));
 
 app.listen(3001);
