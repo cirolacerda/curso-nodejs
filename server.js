3ca0238ec2a64f81caee50029ad77c7932bbd857
Ciro Botelho Lacerda <cirolacerda@gmail.com>
2020-06-13 12:46:09 -0300

    Criando estrutura com mongoDB

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// Iniciando o App
 const app = express();
 
+// Iniciando o MongoDB
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true, useUnifiedTopology:true});
+
+// Primeira rota
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.send("Hello World Ciro");
 });
 
 app.listen(3001);
